62900b6729a0efa33c760e641e451c36925dc275
pereraba_ps14036 <erangakaushan@gmail.com>
2017-12-06 15:03:32 +0530

    qqq

--- src/DBConnect/dbConnect.java
+++ src/DBConnect/dbConnect.java
@@ -5,7 +5,7 @@ public class dbConnect {
         public String url = "172.16.20.180";
         public String port = "3306"; //8889
         public String database = "test";
-        public String user = "admin";
+        public String user = "admin";//root
         public String password = "stcs";//root
         public String JDBC_DRIVER = "com.mysql.jdbc.Driver";
         Connection conn = null;
@@ -28,6 +28,8 @@ public class dbConnect {
         }
 
 
+
+
     public Statement Conn2DB()
     {
         String DB_URL = "jdbc:mysql://"+url+":"+port+"/"+database;
@@ -44,4 +46,15 @@ public class dbConnect {
         return dbstmt;
     }
 
+
+
+    public void closeDB(){
+        try{
+            conn.close();
+            System.out.println("connection close");
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
